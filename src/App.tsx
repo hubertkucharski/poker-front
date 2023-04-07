ca07a0e1444924d1cc37611a68f3e2cb494974e7
hubertkucharski <hubertkucharski@gmail.com>
2023-04-07 11:50:41 +0200

    fix missing active context

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {SingleRoom} from './Components/SingleRoom/SingleRoom';
 import './global.css'
+import {ActivityStore, ActivityStoreContext} from "./stores/activityStore";
 
 function App() {
     return (
+        <ActivityStoreContext.Provider value={new ActivityStore()}>
                 <Router>
                     <Routes>
                         <Route path="/single-room" element={<SingleRoom/>}/>
                         {/*<Route path="/poker-room/:roomId" element={<Chat/>} />*/}
                     </Routes>
                 </Router>
+        </ActivityStoreContext.Provider>
     )
 }
 
