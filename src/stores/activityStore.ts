e05bfbbe4f64c829cb66f08919df288a4e738b33
hubertkucharski <hubertkucharski@gmail.com>
2023-04-18 09:38:24 +0200

    modify backend to save currentState of the room in sql

--- src/stores/activityStore.ts
+++ src/stores/activityStore.ts
@@ -3,8 +3,12 @@ import { createContext } from "react";
 import { Cards, changeCardNaming } from "../Components/utils/changeCardNaming";
 import { CommonCards } from "../types/types";
 
+export interface CurrentState {
+  commonCards: Cards[];
+}
+
 const DEFAULT_COMMON_CARDS = ["", "", "", "", ""];
-const DEFAULT_PLAYER_CARDS = [["", ""]];
+const DEFAULT_PLAYER_CARDS = ["", ""];
 const DEFAULT_PLAYER_INDEX = -1;
 
 export class ActivityStore {
@@ -12,17 +16,23 @@ export class ActivityStore {
     makeAutoObservable(this, {}, { autoBind: true });
   }
   @observable commonCards = DEFAULT_COMMON_CARDS;
-  @observable playersCards: CommonCards[] = DEFAULT_PLAYER_CARDS;
+  @observable playersCards: CommonCards = DEFAULT_PLAYER_CARDS;
   @observable playerIndex: number = DEFAULT_PLAYER_INDEX;
+  @observable currentState: CommonCards = DEFAULT_COMMON_CARDS;
 
   @action setCommonCards(newCards: Cards[]) {
     this.commonCards = changeCardNaming([newCards])[0] as CommonCards;
   }
-  @action setPlayerCards(newPlayersCards: Cards[][]) {
-    this.playersCards = changeCardNaming(newPlayersCards) as CommonCards[];
+  @action setPlayerCards(newPlayersCards: Cards[]) {
+    this.playersCards = changeCardNaming([newPlayersCards]) as CommonCards;
   }
   @action setPlayerIndex(newPlayerIndex: number) {
     this.playerIndex = newPlayerIndex;
+  }
+  @action setCurrentState(newCurrentState: CurrentState) {
+    this.commonCards = changeCardNaming([
+      newCurrentState.commonCards,
+    ])[0] as CommonCards;
   }
 }
 export const ActivityStoreContext = createContext(new ActivityStore());
